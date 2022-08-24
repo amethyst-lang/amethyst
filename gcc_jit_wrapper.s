ff36603c4f7b38ddd2232be48b73ec63c4c4ad83
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-08-24 15:25:28 -0400

    Added break

--- gcc_jit_wrapper.s
+++ gcc_jit_wrapper.s
@@ -2,6 +2,8 @@
 .extern gcc_jit_context_compile_to_file
 .global gcc_jit_block_end_with_conditional_wrapper
 .extern gcc_jit_block_end_with_conditional
+.global gcc_jit_block_end_with_void_return_wrapper
+.extern gcc_jit_block_end_with_void_return
 
 .intel_syntax
 
@@ -18,5 +20,13 @@ gcc_jit_block_end_with_conditional_wrapper:
     sub %rsp, %rax
     call gcc_jit_block_end_with_conditional
     mov %rax, 0x40
+    add %rsp, %rax
+    ret
+
+gcc_jit_block_end_with_void_return_wrapper:
+    mov %rax, 0x30
+    sub %rsp, %rax
+    call gcc_jit_block_end_with_void_return
+    mov %rax, 0x30
     add %rsp, %rax
     ret
