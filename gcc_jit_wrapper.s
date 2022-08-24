ebed34335d4ca90a98f1378881ae91d8440384ff
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-08-24 17:21:54 -0400

    Fixed some bugs

--- gcc_jit_wrapper.s
+++ gcc_jit_wrapper.s
@@ -4,29 +4,39 @@
 .extern gcc_jit_block_end_with_conditional
 .global gcc_jit_block_end_with_void_return_wrapper
 .extern gcc_jit_block_end_with_void_return
+.global gcc_jit_lvalue_as_rvalue_wrapper
+.extern gcc_jit_lvalue_as_rvalue
 
 .intel_syntax
 
 gcc_jit_context_compile_to_file_raw:
-    mov %rax, 0x20
-    sub %rsp, %rax
+    mov %r11, 0x20
+    sub %rsp, %r11
     call gcc_jit_context_compile_to_file
-    mov %rax, 0x20
-    add %rsp, %rax
+    mov %r11, 0x20
+    add %rsp, %r11
     ret
 
 gcc_jit_block_end_with_conditional_wrapper:
-    mov %rax, 0x40
-    sub %rsp, %rax
+    mov %r11, 0x40
+    sub %rsp, %r11
     call gcc_jit_block_end_with_conditional
-    mov %rax, 0x40
-    add %rsp, %rax
+    mov %r11, 0x40
+    add %rsp, %r11
     ret
 
 gcc_jit_block_end_with_void_return_wrapper:
-    mov %rax, 0x30
-    sub %rsp, %rax
+    mov %r11, 0x30
+    sub %rsp, %r11
     call gcc_jit_block_end_with_void_return
-    mov %rax, 0x30
-    add %rsp, %rax
+    mov %r11, 0x30
+    add %rsp, %r11
+    ret
+
+gcc_jit_lvalue_as_rvalue_wrapper:
+    mov %r11, 0x30
+    sub %rsp, %r11
+    call gcc_jit_lvalue_as_rvalue
+    mov %r11, 0x30
+    add %rsp, %r11
     ret
