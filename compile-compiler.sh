77f6905da4a97bae636c234302dbcfe67c487c9c
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-10-07 17:35:10 -0400

    Made compiler output object file

--- compile-compiler.sh
+++ compile-compiler.sh
@@ -2,4 +2,4 @@
 nasm -f elf64 _start.s -o _start.o
 gcc -c llvm_wrapper.s -o llvm_wrapper.o
 ./target/release/amethyst self-hosted-compiler/compiler.amy amethystc
-gcc _start.o amethystc.o llvm_wrapper.o `llvm-config --ldflags --cflags --system-libs --libs all` -nostdlib -o amethystc
+gcc _start.o amethystc.o llvm_wrapper.o `llvm-config --ldflags --system-libs --libs all` -nostartfiles -o amethystc
--- llvm_wrapper.s
+++ llvm_wrapper.s
@@ -6,6 +6,12 @@
 .extern LLVMFunctionType
 .global LLVMConstIntWrapper
 .extern LLVMConstInt
+.global LLVMInitializeX86TargetMCWrapper
+.extern LLVMInitializeX86TargetMC
+.global LLVMCreateTargetMachineWrapper
+.extern LLVMCreateTargetMachine
+.global LLVMTargetMachineEmitToFileWrapper
+.extern LLVMTargetMachineEmitToFile
 
 .intel_syntax
 
@@ -23,4 +29,22 @@ LLVMFunctionTypeWrapper:
 
 LLVMConstIntWrapper:
     call LLVMConstInt
+    ret
+
+LLVMInitializeX86TargetMCWrapper:
+    call LLVMInitializeX86TargetMC
+    ret
+
+LLVMCreateTargetMachineWrapper:
+    mov %r11, 0
+    push %r11
+    mov %r11, [%rsp + 0x10]
+    push %r11
+    call LLVMCreateTargetMachine
+    pop %r11
+    pop %r11
+    ret
+
+LLVMTargetMachineEmitToFileWrapper:
+    call LLVMTargetMachineEmitToFile
     ret
