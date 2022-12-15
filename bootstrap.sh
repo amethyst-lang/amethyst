65ead32aef76c8137aa6388282b92c653e813d0b
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2022-12-15 18:08:25 -0500

    Stopped work on self hosted compiler for now

--- bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./amethystc self-hosted-compiler/compiler.amy
-gcc a.out.o `llvm-config --ldflags --system-libs --libs all`
--- compile-compiler.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./target/release/amethyst self-hosted-compiler/compiler.amy amethystc
-gcc amethystc.o `llvm-config --ldflags --system-libs --libs all` -o amethystc
