0bbd82f69ff0208daacca84290168d904ec5670b
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2023-01-09 16:47:41 -0500

    a

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use std::collections::HashMap;
 
+/*
 use amethyst::backend::arch::rv64::RvSelector;
 use amethyst::backend::arch::x64::X64Selector;
 use amethyst::backend::regalloc::RegAlloc;
 use amethyst::backend::sexpr_lowering;
+*/
 use amethyst::frontend::{ast_lowering, correctness, macros};
 use amethyst::parser::TopParser;
 
 fn main() {
     let contents = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
-    let target = std::env::args().nth(2).unwrap_or_else(|| String::from("x64"));
+    //let target = std::env::args().nth(2).unwrap_or_else(|| String::from("x64"));
 
     let mut asts = TopParser::new().parse(&contents).unwrap();
     let mut map = HashMap::new();
