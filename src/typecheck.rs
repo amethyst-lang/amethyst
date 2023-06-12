f7930e5a2095dfef8212ad5aaf516505294f39af
jenra-uwu <24357776+jenra-uwu@users.noreply.github.com>
2023-06-12 02:36:34 +0000

    Added match

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use crate::{lexer::Span, parse::{Ast, Type, Monotype, BaseType}};
+use crate::{lexer::Span, parse::{Ast, Type, Monotype, BaseType, Pattern}};
 
 #[derive(Debug)]
 pub struct CheckError {
@@ -15,6 +15,7 @@ pub struct CheckError {
 struct Environment {
     variables: Vec<(String, Type)>,
     substitutions: Vec<Monotype>,
+    constructors: HashMap<String, Type>,
 }
 
 impl Environment {
@@ -39,6 +40,10 @@ impl Environment {
         }
 
         return None;
+    }
+
+    fn find_cons(&mut self, cons: &str) -> Option<Monotype> {
+        self.constructors.get(cons).cloned().map(|v| v.instantiate(self))
     }
 
     fn pop_var(&mut self) -> Option<(String, Type)> {
@@ -272,9 +277,205 @@ fn typecheck_helper(ast: &mut Ast, env: &mut Environment, errors: &mut Vec<Check
 
         Ast::DatatypeDefinition { .. } => Monotype::Base(BaseType::Bottom),
 
-        Ast::Match { span, value, patterns } => todo!(),
+        Ast::Match { span, value, patterns } => {
+            let mut t_value = typecheck_helper(value, env, errors);
+            let mut t = env.new_var();
+
+            for (pat, body) in patterns.iter_mut() {
+                let (mut t_pat, vars) = typecheck_pattern(pat, env, errors);
+                if !env.unify(&mut t_pat, &mut t_value) {
+                    errors.push(CheckError {
+                        message: "incompatible types among pattern and value".to_string(),
+                        primary_label: format!("pattern has type `{}` but value has incompatible type `{}`", t_pat, t_value),
+                        primary_label_loc: pat.span(),
+                        secondary_labels: Vec::new(),
+                        notes: Vec::new(),
+                    });
+                }
+
+                for (var, monotype) in vars.iter().cloned() {
+                    env.push_var(var, Type {
+                        foralls: Vec::new(),
+                        constraints: Vec::new(),
+                        monotype,
+                    });
+                }
+
+                let mut t_body = typecheck_helper(body, env, errors);
+                if !env.unify(&mut t, &mut t_body) {
+                    errors.push(CheckError {
+                        message: "match expression has incompatible types in branches".to_string(),
+                        primary_label: format!("two branches have types `{}` and `{}`", t, t_body),
+                        primary_label_loc: span.clone(),
+                        secondary_labels: Vec::new(),
+                        notes: Vec::new(),
+                    });
+                }
+
+                for _ in vars {
+                    env.pop_var();
+                }
+            }
+
+            t
+        }
+
         Ast::Class { span, name, generics, constraints, functions } => todo!(),
         Ast::Instance { span, name, generics, parameters, constraints, functions } => todo!(),
+    }
+}
+
+fn typecheck_pattern(pattern: &mut Pattern, env: &mut Environment, errors: &mut Vec<CheckError>) -> (Monotype, Vec<(String, Monotype)>) {
+    match pattern {
+        Pattern::Wildcard(_) => (Monotype::Base(BaseType::Bottom), Vec::new()),
+
+        Pattern::Symbol(_, s) => {
+            let t = env.new_var();
+            (t.clone(), vec![(s.clone(), t)])
+        }
+
+        Pattern::Constructor(span, cons, pats) => {
+            if let Some(mut t_cons) = env.find_cons(cons) {
+                let mut new_env = Vec::new();
+                for pat in pats {
+                    let (mut t_pat, addition) = typecheck_pattern(pat, env, errors);
+
+                    for new in addition {
+                        if new_env.iter().any(|(v, _)| *v == new.0) {
+                            errors.push(CheckError {
+                                message: "variable declared multiple times in pattern".to_string(),
+                                primary_label: format!("variable `{}` declared at least twice", new.0),
+                                primary_label_loc: span.clone(),
+                                secondary_labels: Vec::new(),
+                                notes: Vec::new(),
+                            });
+                            continue;
+                        }
+
+                        new_env.push(new);
+                    }
+
+                    if let Monotype::Func(mut t_arg, t_ret) = t_cons {
+                        if !env.unify(&mut t_pat, &mut t_arg) {
+                            errors.push(CheckError {
+                                message: "pattern type does not match constructor".to_string(),
+                                primary_label: format!("constructor argument has type `{}` but type `{}` was provided", t_arg, t_pat),
+                                primary_label_loc: span.clone(),
+                                secondary_labels: Vec::new(),
+                                notes: Vec::new(),
+                            });
+                        }
+                        t_cons = *t_ret;
+                    } else {
+                        errors.push(CheckError {
+                            message: "too few arguments in pattern".to_string(),
+                            primary_label: "constructor has more arguments than pattern".to_string(),
+                            primary_label_loc: span.clone(),
+                            secondary_labels: Vec::new(),
+                            notes: Vec::new(),
+                        });
+                        return (Monotype::Base(BaseType::Bottom), Vec::new());
+                    }
+                }
+
+                if let Monotype::Func(_, _) = t_cons {
+                    errors.push(CheckError {
+                        message: "too many arguments in pattern".to_string(),
+                        primary_label: "constructor has fewer arguments than pattern".to_string(),
+                        primary_label_loc: span.clone(),
+                        secondary_labels: Vec::new(),
+                        notes: Vec::new(),
+                    });
+                    (Monotype::Base(BaseType::Bottom), new_env)
+                } else {
+                    (t_cons, new_env)
+                }
+            } else {
+                (Monotype::Base(BaseType::Bottom), Vec::new())
+            }
+        }
+
+        Pattern::SymbolOrUnitConstructor(_, name) => {
+            if let Some(t_cons) = env.find_cons(name) {
+                if let Monotype::Func(_, _) = t_cons {
+                    let t = env.new_var();
+                    (t.clone(), vec![(name.clone(), t)])
+                } else {
+                    (t_cons, Vec::new())
+                }
+            } else {
+                let t = env.new_var();
+                (t.clone(), vec![(name.clone(), t)])
+            }
+        }
+
+        Pattern::As(span, name, pat) => {
+            let (t_pat, mut new_env) = typecheck_pattern(pat, env, errors);
+            if new_env.iter().any(|(v, _)| v == name) {
+                errors.push(CheckError {
+                    message: "variable declared multiple times in pattern".to_string(),
+                    primary_label: format!("variable `{}` declared at least twice", name),
+                    primary_label_loc: span.clone(),
+                    secondary_labels: Vec::new(),
+                    notes: Vec::new(),
+                });
+            } else {
+                new_env.push((name.clone(), t_pat.clone()));
+            }
+
+            (t_pat, new_env)
+        }
+
+        Pattern::Or(span, cases) => {
+            let mut t = Monotype::Base(BaseType::Bottom);
+            let mut new_env = Vec::new();
+            let mut first = true;
+            for case in cases {
+                if first {
+                    (t, new_env) = typecheck_pattern(case, env, errors);
+                    first = false;
+                    continue;
+                }
+
+                let (mut t_case, mut variant) = typecheck_pattern(case, env, errors);
+                if !env.unify(&mut t, &mut t_case) {
+                    errors.push(CheckError {
+                        message: "or pattern has incompatible types".to_string(),
+                        primary_label: format!("found incompatible types `{}` and `{}`", t, t_case),
+                        primary_label_loc: span.clone(),
+                        secondary_labels: Vec::new(),
+                        notes: Vec::new(),
+                    });
+                    return (Monotype::Base(BaseType::Bottom), Vec::new())
+                }
+
+                if variant.len() != new_env.len() {
+                    errors.push(CheckError {
+                        message: "incompatible variable count".to_string(),
+                        primary_label: "or pattern has inconsistent number of variables".to_string(),
+                        primary_label_loc: span.clone(),
+                        secondary_labels: Vec::new(),
+                        notes: Vec::new(),
+                    });
+                    return (Monotype::Base(BaseType::Bottom), Vec::new())
+                }
+
+                for (n1, t1) in new_env.iter_mut() {
+                    if !variant.iter_mut().any(|(n2, t2)| n1 == n2 && env.unify(t1, t2)) {
+                        errors.push(CheckError {
+                            message: "unshared variable found in or pattern".to_string(),
+                            primary_label: format!("variable `{}` not shared amongst patterns", n1),
+                            primary_label_loc: span.clone(),
+                            secondary_labels: Vec::new(),
+                            notes: Vec::new(),
+                        });
+                        return (Monotype::Base(BaseType::Bottom), Vec::new())
+                    }
+                }
+            }
+
+            (t, new_env)
+        }
     }
 }
 
@@ -299,11 +500,13 @@ pub fn typecheck(asts: &mut [Ast]) -> Result<(), Vec<CheckError>> {
                         monotype = Monotype::Func(Box::new(field_type.clone()), Box::new(monotype));
                     }
 
-                    env.push_var(cons.clone(), Type {
+                    let type_ = Type {
                         foralls: t.foralls.clone(),
                         constraints: t.constraints.clone(),
                         monotype,
-                    });
+                    };
+                    env.constructors.insert(cons.clone(), type_.clone());
+                    env.push_var(cons.clone(), type_);
                 }
             }
 
